c94e619ac204c5d7e7f74a16c173cb20fbd261d3
Yoo JIeun <yoo.jieun@cashslide.co.kr>
2015-11-04 13:14:52 +0900

    First Commit (Splash, Main page)

---
+++ app/src/main/java/com/yoojieun/memoji/SplashScreen.java
@@ -0,0 +1,64 @@
+package com.yoojieun.memoji;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * Created by yoojieun on 2015. 10. 27..
+ */
+
+public class SplashScreen extends Activity{
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Log.v("memoji", "handleMessage msg.what = "+ msg.what );
+            if(msg.what == 100){
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        mHandler.sendEmptyMessageDelayed(100,3000);
+
+//        Thread timerThread = new Thread() {
+//            public void run() {
+//                try {
+//                    sleep(3000);
+//                }catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }finally {
+//                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+//                    startActivity(intent);
+//                }
+//            }
+//        };
+//        timerThread.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeMessages(100);
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
